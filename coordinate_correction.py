c13b5e699929fb46cefaeb995b8e40998d1026e0
João Carlos <82067146+joao-carlos-eng@users.noreply.github.com>
2023-04-15 16:40:26 -0300

    implementação e correção dos testes de coordinate_correction
    As alterações feitas foram:
    
    Correção de um erro na função apply_osnap que resultava em um TypeError
    Modificação na função apply_osnap para permitir que os pontos de referência sejam postes ou caixas
    Adição de uma verificação para remover as posições vazias dos postes corrigidos
    Adição de uma verificação para remover as posições vazias dos cabos corrigidos
    Adição de prints para debug nos loops de correção de caixas e cabos

--- coordinate_correction.py
+++ coordinate_correction.py
@@ -1,58 +1,100 @@
 # calcular a distancia entre dois pontos
-import math
 
 
-def distancia_dois_pontos(ponto1, ponto2):
-    x1, y1, z1 = ponto1
-    x2, y2, z2 = ponto2
-
-    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+from math import sin, cos, sqrt, atan2, radians
 
 
-def correct_coordinates(postes, caixas, trajetos, raio):
+def distancia_dois_pontos(lat1, lon1, lat2, lon2):
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
+
+    # Raio da Terra em metros
+    R = 6371000
+
+    # Converte as coordenadas para radianos
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    # Calcula as diferenças de latitude e longitude
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    # Calcula a distância entre os pontos
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance = round(R * c)
+
+    return round(distance)
+
+
+def correct_coordinates(postes, caixas, cabos, raio):
     """
     Corrige as coordenadas das caixas e dos trajetos dos postes utilizando o método OSNAP.
 
     Args:
         postes (list): Lista de coordenadas dos postes.
         caixas (list): Lista de coordenadas das caixas.
-        trajetos (list): Lista de coordenadas dos trajetos.
+        cabos (list): Lista de coordenadas dos cabos.
         raio (float): Raio de aplicação do método OSNAP.
 
     Returns:
         None
     """
 
-    def apply_osnap(lista, string, raio):
+    def apply_osnap(pontos_referencia, cord_a_corrigir, raio):
         """
         Aplica o método OSNAP em uma string de coordenadas.
 
         Args:
-            lista (list): Lista de coordenadas a serem comparadas.
-            string (str): String de coordenadas a serem corrigidas.
+            pontos_referencia (list): Lista de dicionário dos postes com coordenadas para referência.
+            cord_a_corrigir (dict): Dicionario do ponto ou cabo com da coordenada a serem corrigidas.
             raio (float): Raio de aplicação do método OSNAP.
 
         Returns:
             str: String de coordenadas corrigidas pelo método OSNAP.
         """
-        out = ''
-        lista2 = string.split()
-        for i in lista2:
-            out = out + f'{i} '
+        string_coordinates = cord_a_corrigir['coordinates']
+        list_cords_corrigir = string_coordinates.split(' ')
 
-        for i in lista2:
-            x = i.split(',')
-            for p in lista:
-                if distancia_dois_pontos(p, x) <= raio:
-                    out = out.replace(f'{i}', f'{p[0]},{p[1]},{p[2]}')
-        return out
+        if cord_a_corrigir.get('postes') is not None:
+            key = 'postes'
+        else:
+            key = 'poste'
+
+        for cord_n, cord in enumerate(list_cords_corrigir):  # para cada cordenada a ser corrigida (cord)
+            x = cord.split(',')
+            for p_num, p in enumerate(pontos_referencia):  # para cada poste de referencia (p)
+                co = p['coordinates'].split(',')
+                if distancia_dois_pontos(co[0], co[1], x[0], x[1]) <= raio:
+                    list_cords_corrigir[cord_n] = p['coordinates']
+                    if key == 'postes':
+                        cord_a_corrigir[key][cord_n] = pontos_referencia[p_num]['name']
+                    else:
+                        cord_a_corrigir[key] = pontos_referencia[p_num]['name']
+                    break
+
+        cord_corrigido = []
+        for a, b in zip(list_cords_corrigir, cord_a_corrigir[key]):
+            if b is not None:
+                cord_corrigido.append(a)
+
+        if len(cord_corrigido) == 1:
+            return cord_corrigido[0]
+        return ' '.join(cord_corrigido).strip()
 
     # Corrigir as coordenadas das caixas
     for caixa in caixas:
-        caixa['coordenadas'] = apply_osnap(postes, caixa['coordenadas'], raio)
+        print('testando caixa: ', caixa['name'])
+        caixa['poste'] = None
+        caixa['coordinates'] = apply_osnap(postes, caixa, raio)
 
-    # Corrigir o trajeto dos postes
-    for trajeto in trajetos:
-        trajeto['coordenadas'] = apply_osnap(
-            postes, trajeto['coordenadas'], raio
-        )
+    # Corrigir o trajeto dos cabos
+    for cabo in cabos:
+        print('testando cabo: ', cabo['name'])
+        cabo['postes'] = [None for _ in range(len(cabo['coordinates'].split(' ')))]
+        cabo['coordinates'] = apply_osnap(postes, cabo, raio)
+        cabo['postes'] = [p for p in cabo['postes'] if p is not None]
