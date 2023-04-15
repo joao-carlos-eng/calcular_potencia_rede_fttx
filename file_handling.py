e87efff382b6d7033cea59577f8a9a4ad7b7266a
Joao Carlos <joao.carlos.dev.log@gmail.com>
2023-04-14 21:08:31 -0300

    Update file_handling.py
    criado docstrings

--- file_handling.py
+++ file_handling.py
@@ -1,7 +1,17 @@
 from pykml import parser
 
-# Função para ler o arquivo KML e extrair as informações
+
 def read_kml(file_path):
+    """
+    Lê o arquivo KML no caminho especificado e retorna um objeto Python contendo as informações extraídas.
+
+    :param file_path: str, caminho do arquivo KML a ser lido
+    :return: Objeto Python contendo as informações do arquivo KML processado
+
+    Exemplo de uso:
+
+    kml = read_kml("exemplo-rede-fttx.kml")
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         kml_content = file.read()
     kml_content = kml_content.replace(
