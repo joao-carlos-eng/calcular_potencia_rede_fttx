c1981ffdf57a58e9c0cddaa31cd7f3ec582a35e5
Joao Carlos <joao.carlos.dev.log@gmail.com>
2023-04-25 18:02:18 -0300

    Update coordinate_correction.py

--- coordinate_correction.py
+++ coordinate_correction.py
@@ -1,5 +1,45 @@
 # modulo coordinate_correction.py
-from math import atan2, cos, radians, sin, sqrt
+from math import sqrt
+import simplekml as kml
+
+
+def apply_osnap(pontos_referencia, Place_a_corrigir, raio):
+    """
+    Aplica o método OSNAP em uma string de coordenadas.
+
+    Args:
+        pontos_referencia (list): Lista de dicionário dos postes com coordenadas para referência.
+        Place_a_corrigir (dict): Dicionario do ponto ou cabo com da coordenada a serem corrigidas.
+        raio (float): Raio de aplicação do método OSNAP.
+
+    Returns:
+        str: String de coordenadas corrigidas pelo método OSNAP.
+    """
+    list_cords_corrigir = Place_a_corrigir['coordinates']
+    if Place_a_corrigir.get('postes') is not None:
+        key = 'postes'
+    else:
+        key = 'poste'
+
+    for cord_n, cord in enumerate(list_cords_corrigir):  # para cada coordenada a ser corrigida (cord)
+        for p_num, p in enumerate(pontos_referencia):  # para cada poste de referencia (p)
+            if distancia_dois_pontos(p['coordinates'][0], cord) <= raio:
+                list_cords_corrigir[cord_n] = p['coordinates'][0]
+                Place_a_corrigir[key][cord_n] = pontos_referencia[p_num]['name']
+                break
+        if Place_a_corrigir[key] is None:
+            lat_lon = cord.split(',')[1] + ',' + cord.split(',')[0]
+            print('Nenhum poste encontrado para a coordenada: ', lat_lon, 'do objeto: ', Place_a_corrigir['name'],
+                  "verifique se a caixa está pocicionada corretamente ou tente um raio maior.")
+            return
+
+    cord_corrigido = []
+
+    for a, b in zip(list_cords_corrigir, Place_a_corrigir[key]):
+        if b is not None:
+            cord_corrigido.append(a)
+
+    return cord_corrigido
 
 
 def distancia_dois_pontos(coord1, coord2):
@@ -22,6 +62,7 @@ def distancia_dois_pontos(coord1, coord2):
 
 
 def correct_coordinates(postes, caixas, cabos, raio):
+    auditoria = kml.Kml(name='Auditoria')
     """
     Corrige as coordenadas das caixas e dos trajetos dos postes utilizando o método OSNAP.
 
@@ -35,58 +76,26 @@ def correct_coordinates(postes, caixas, cabos, raio):
         None
     """
 
-    def apply_osnap(pontos_referencia, cord_a_corrigir, raio):
-        """
-        Aplica o método OSNAP em uma string de coordenadas.
-
-        Args:
-            pontos_referencia (list): Lista de dicionário dos postes com coordenadas para referência.
-            cord_a_corrigir (dict): Dicionario do ponto ou cabo com da coordenada a serem corrigidas.
-            raio (float): Raio de aplicação do método OSNAP.
-
-        Returns:
-            str: String de coordenadas corrigidas pelo método OSNAP.
-        """
-        string_coordinates = cord_a_corrigir['coordinates']
-        list_cords_corrigir = string_coordinates.split(' ')
-
-        if cord_a_corrigir.get('postes') is not None:
-            key = 'postes'
-        else:
-            key = 'poste'
-
-        for cord_n, cord in enumerate(list_cords_corrigir):  # para cada cordenada a ser corrigida (cord)
-            for p_num, p in enumerate(pontos_referencia):  # para cada poste de referencia (p)
-                if distancia_dois_pontos(p['coordinates'], cord) <= raio:
-                    list_cords_corrigir[cord_n] = p['coordinates']
-                    if key == 'postes':
-                        cord_a_corrigir[key][cord_n] = pontos_referencia[p_num]['name']
-                    else:
-                        cord_a_corrigir[key] = pontos_referencia[p_num]['name']
-                    break
-            if cord_a_corrigir[key] is None:
-                print('Nenhum poste encontrado para a coordenada: ', cord, 'do objeto: ', cord_a_corrigir['name'])
-                return
-
-        cord_corrigido = []
-
-        for a, b in zip(list_cords_corrigir, cord_a_corrigir[key]):
-            if b is not None:
-                cord_corrigido.append(a)
-
-        if len(cord_corrigido) == 1:
-            return cord_corrigido[0]
-        return ' '.join(cord_corrigido).strip()
-
     # Corrigir as coordenadas das caixas
+    kml_caixas = auditoria.newfolder(name='Caixas')
     for caixa in caixas:
-        print('testando caixa: ', caixa['name'])
-        caixa['poste'] = None
+        caixa['poste'] = [None]
         caixa['coordinates'] = apply_osnap(postes, caixa, raio)
+        co = caixa['coordinates'][0].split(',')
+        kml_caixas.newpoint(name=caixa['name'], coords=[(float(co[0]), float(co[1]))])
 
     # Corrigir o trajeto dos cabos
+    kml_cabos = auditoria.newfolder(name='Cabos')
     for cabo in cabos:
-        print('testando cabo: ', cabo['name'])
-        cabo['postes'] = [None for _ in range(len(cabo['coordinates'].split(' ')))]
+        cabo['postes'] = [None for _ in range(len(cabo['coordinates']))]
         cabo['coordinates'] = apply_osnap(postes, cabo, raio)
         cabo['postes'] = [p for p in cabo['postes'] if p is not None]
+        co = cabo['coordinates']
+        co = [(float(c.split(',')[0]), float(c.split(',')[1])) for c in co]
+        kml_cabos.newlinestring(name=cabo['name'], coords=co)
+
+    kml_postes = auditoria.newfolder(name='Postes')
+    for poste in postes:
+        co = poste['coordinates'][0].split(',')
+        kml_postes.newpoint(name=poste['name'], coords=[(float(co[0]), float(co[1]))])
+    auditoria.save('auditoria.kml')
